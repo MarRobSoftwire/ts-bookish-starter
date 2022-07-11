3c69656e0b417d6d5cf381bda6b6ea51413a53b0
MarRobSoftwire <mark.Roberts@softwire.com>
2022-07-11 16:59:21 +0100

    add createBook functionality

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -25,7 +25,10 @@ class BookController {
         this.router = Router();
         this.router.get('/:id', this.getBook.bind(this));
         this.router.get('/', this.getBooks.bind(this));
-        this.router.post('/', this.createBook.bind(this));
+        this.router.post(
+            '/:bookTitle/:copies/:ISBN/:authorFirst/:authorLast',
+            this.createBook.bind(this),
+        );
     }
 
     async getBook(req: Request, res: Response) {
@@ -64,6 +67,52 @@ class BookController {
         connection.connect();
     }
 
+    async createBook(req: Request, res: Response) {
+        const connection = new Connection(this.config);
+        let requestStatement = new String(); //Plz ignore
+        const output = [];
+
+        // Setup event handler when the connection is established.
+        connection.on('connect', async function (err) {
+            if (err) {
+                console.log('Error: ', err);
+                throw err;
+            }
+            const parsedBookTitle = req.params.bookTitle.split('_').join(' ');
+            const sqlRequest =
+                // eslint-disable-next-line quotes
+                "INSERT INTO books (BookTitle, Copies, ISBN) VALUES ( '" +
+                parsedBookTitle +
+                // eslint-disable-next-line quotes
+                "', " +
+                req.params.copies +
+                ',' +
+                req.params.ISBN +
+                // eslint-disable-next-line quotes
+                ") INSERT INTO authors (FirstName, Surname) VALUES ( '" +
+                req.params.authorFirst +
+                // eslint-disable-next-line quotes
+                "', '" +
+                req.params.authorLast +
+                // eslint-disable-next-line quotes
+                "') INSERT INTO books_authors (Author_Id, Book_Id) VALUES ( IDENT_CURRENT('authors'), IDENT_CURRENT('books')) ";
+                console.log(sqlRequest)
+            const databaseRequest = new TediousRequest(sqlRequest, function (
+                err,
+            ) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    return res
+                        .status(200)
+                        .json({ status: 'Book has been added' });
+                }
+            });
+            requestStatement = connection.execSql(databaseRequest);
+        });
+        connection.connect();
+    }
+
     async getBooks(req: Request, res: Response) {
         const connection = new Connection(this.config);
         const output = [];
@@ -94,42 +143,6 @@ class BookController {
                 },
             );
             connection.execSql(databaseRequest);
-        });
-        connection.connect();
-    }
-
-    async createBook(req: Request, res: Response) {
-        const connection = new Connection(this.config);
-        let requestStatement = new String(); //Plz ignore
-        const output = [];
-
-        // Setup event handler when the connection is established.
-        connection.on('connect', async function (err) {
-            if (err) {
-                console.log('Error: ', err);
-                throw err;
-            }
-            const databaseRequest = new TediousRequest(
-                'SELECT * FROM books WHERE id = ' + req.params.id,
-                function (err, rowCount, rows) {
-                    if (err) {
-                        console.log(err);
-                        throw err;
-                    } else {
-                        if (rows.length == 0) {
-                            return res
-                                .status(400)
-                                .json({ status: 'book not found' });
-                        } else {
-                            for (let j = 0; j < rows[0].length; j++) {
-                                output.push(rows[0][j].value);
-                            }
-                            return res.status(200).json(output);
-                        }
-                    }
-                },
-            );
-            requestStatement = connection.execSql(databaseRequest);
         });
         connection.connect();
     }
